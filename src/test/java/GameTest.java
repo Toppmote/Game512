a0aa50aaa40b0ea59602d9aa97c7891bfb74adda
Pavel Chupikov <toppmote@gmail.com>
2022-04-02 20:38:45 +0700

    Добавил тест инициализации класса игры

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,19 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+
+    /**
+     * Тест инициализации класса игры.
+     * Проверяем, что сам класс не null, игровое поле не null и количество очков == 0
+     */
+    @Test
+    void initGameClassTest() {
+        Game game = new Game();
+        assertNotNull(game);
+        assertNotNull(game.getField());
+        assertEquals(0, game.getPoints());
+    }
+
 }
