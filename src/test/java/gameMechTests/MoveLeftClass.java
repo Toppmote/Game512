0ea7d9080a596d644bcbd06d791e344471c739d5
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 17:21:28 +0700

    Добавил класс для тестирования операции перемещения всех цифр на игровом поле влево.

---
+++ src/test/java/gameMechTests/MoveLeftClass.java
@@ -0,0 +1,14 @@
+package gameMechTests;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс для тестирования метода перемещения всех цифр на игровом поле влево.
+ */
+public class MoveLeftClass {
+
+
+
+}
