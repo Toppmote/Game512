a751b129cb3a8e3c0c54d3c4a5503ef92870e6f9
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 16:49:58 +0700

    Добавил тест для проверки метода вывода правил игры

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,8 +1,26 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
+    }
 
     /**
      * Тест инициализации класса игры.
@@ -14,6 +32,22 @@ public class GameTest {
         assertNotNull(game);
         assertNotNull(game.getField());
         assertEquals(0, game.getPoints());
+    }
+
+    /**
+     * Тест печати правил игры перед её запуском.
+     * Проверяем, что печатается в консоль.
+     * Т.к. метод приватный, запускаем его через рефлексию.
+     */
+    @Test
+    void printRulesTest() throws Exception {
+        final String RULES_STRING = "Game 512.\n" + "Двигайте цифры вверх вниз влево или право соединяя одинаковые цифры." +
+                "Как только значение одной клетки достигнет 512 - вы победили. Если вы не сможете больше двигать цифры - " +
+                "вы проиграли.\n";
+        Method printRules = Game.class.getDeclaredMethod("printRules");
+        printRules.setAccessible(true);
+        printRules.invoke(new Game());
+        assertEquals(outputStreamCaptor.toString(), RULES_STRING);
     }
 
 }
