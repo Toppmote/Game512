af2ab163fd896830d86858fc621e1ddcfa216e28
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 18:54:41 +0700

    Добавил класс для теста служебных методов

---
+++ src/test/java/UtilMethodsTest.java
@@ -0,0 +1,13 @@
+import game.Game;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class UtilMethodsTest {
+
+
+
+}
