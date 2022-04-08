46877c125e9bf7686688a3325ef8924893160d2f
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 20:39:38 +0700

    Добавил тесты для проверки игрового поля на выигрыш

---
+++ src/test/java/gameMechTests/checkTests/CheckWinTest.java
@@ -0,0 +1,57 @@
+package gameMechTests.checkTests;
+
+import game.Game;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс для тестирования проверки игрового поля на выигрыш
+ */
+public class CheckWinTest {
+
+    /**
+     * Тест метода проверки игрового поля с выигрышом на выигрыш.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void checkWinPosition() throws Exception {
+        int[][] winArray = new int[][]{
+                {512, 0, 8, 0},
+                {4, 8, 0, 0},
+                {4, 0, 8, 0},
+                {4, 8, 0, 0}
+        };
+        Game game = new Game();
+        game.setField(winArray);
+        Method checkWin = Game.class.getDeclaredMethod("checkWin");
+        checkWin.setAccessible(true);
+        boolean methodResult = (boolean) checkWin.invoke(game);
+        assertTrue(methodResult);
+    }
+
+    /**
+     * Тест метода проверки игрового поля без выигрыша на выигрыш.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void checkPositionWithoutWin() throws Exception {
+        int[][] arrayWithoutWin = new int[][]{
+                {512, 0, 8, 0},
+                {4, 8, 0, 0},
+                {4, 0, 8, 0},
+                {4, 8, 0, 0}
+        };
+        Game game = new Game();
+        game.setField(arrayWithoutWin);
+        Method checkWin = Game.class.getDeclaredMethod("checkWin");
+        checkWin.setAccessible(true);
+        boolean methodResult = (boolean) checkWin.invoke(game);
+        assertFalse(methodResult);
+    }
+
+}
