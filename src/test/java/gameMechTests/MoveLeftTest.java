ed5d9148bbd21c00797f4971e5991e1b0a9dc33f
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 17:24:12 +0700

    Изменил имя класса с тестами метода для перемещения цифр влево.

--- src/test/java/gameMechTests/MoveLeftTest.java
+++ src/test/java/gameMechTests/MoveLeftTest.java
@@ -2,13 +2,14 @@ package gameMechTests;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 /**
  * Класс для тестирования метода перемещения всех цифр на игровом поле влево.
  */
-public class MoveLeftClass {
+public class MoveLeftTest {
 
+    @Test
+    void testEmptyField() {
 
+    }
 
 }
