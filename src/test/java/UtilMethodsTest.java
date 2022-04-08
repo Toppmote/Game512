8fe91e3a2ab7c184411cb0633b36c914ebcfeab7
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 19:17:04 +0700

    Добавил тесты для служебных методов

--- src/test/java/UtilMethodsTest.java
+++ src/test/java/UtilMethodsTest.java
@@ -5,9 +5,90 @@ import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
+/**
+ * Класс для тестирования служебных методов
+ */
 public class UtilMethodsTest {
 
+    /**
+     * Тест метода копирования столбца игрового поля.
+     * Т.к. метод приватный, тестируем его через рефлексию.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void testCopyColumnMethod() throws Exception {
+        int[][] startArray = new int[][]{
+                {4, 0, 0, 0},
+                {4, 0, 0, 0},
+                {4, 0, 0, 0},
+                {4, 0, 0, 0}
+        };
+        Game game = new Game();
+        game.setField(startArray);
+        Method copyColumn = Game.class.getDeclaredMethod("copyColumn", int.class);
+        copyColumn.setAccessible(true);
+        int[] result = (int[]) copyColumn.invoke(game, 0);
+        int[] expectedArray = new int[]{4, 4, 4, 4};
+        assertArrayEquals(expectedArray, result);
+    }
 
+    /**
+     * Тест метода вставки столбца в игровое поле.
+     * Т.к. метод приватный, тестируем его через рефлексию.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void testInsertColumnMethod() throws Exception {
+        Game game = new Game();
+        int[] insertableColumn = new int[]{8, 8, 8, 8};
+        Method insertColumn = Game.class.getDeclaredMethod("insertColumn", int[].class, int.class);
+        insertColumn.setAccessible(true);
+        insertColumn.invoke(game, insertableColumn, 0);
+        int[][] expectedArray = new int[][]{
+                {8, 0, 0, 0},
+                {8, 0, 0, 0},
+                {8, 0, 0, 0},
+                {8, 0, 0, 0}
+        };
+        assertArrayEquals(expectedArray, game.getField());
+    }
+
+    /**
+     * Тест метода перемещения чисел в пустом одномерном массиве влево.
+     * Т.к. метод приватный, тестируем его через рефлексию.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void testSlideLeftWithEmptyArray() throws Exception {
+        Game game = new Game();
+        int[] emptyArray = new int[]{0, 0, 0, 0};
+        Method slideLeft = Game.class.getDeclaredMethod("slideLeft", int[].class);
+        int[] expectedArray = new int[]{0, 0, 0, 0};
+        slideLeft.setAccessible(true);
+        boolean methodResult = (boolean) slideLeft.invoke(game, emptyArray);
+        assertArrayEquals(emptyArray, expectedArray);
+        assertFalse(methodResult);
+    }
+
+    /**
+     * Тест метода перемещения чисел в одномерном массиве влево.
+     * Т.к. метод приватный, тестируем его через рефлексию.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void testSlideLeftWithSlide() throws Exception {
+        Game game = new Game();
+        int[] arrayForSlide = new int[]{4, 4, 4, 4};
+        Method slideLeft = Game.class.getDeclaredMethod("slideLeft", int[].class);
+        int[] expectedArray = new int[]{8, 8, 0, 0};
+        slideLeft.setAccessible(true);
+        boolean methodResult = (boolean) slideLeft.invoke(game, arrayForSlide);
+        assertArrayEquals(arrayForSlide, expectedArray);
+        assertTrue(methodResult);
+    }
 
 }
