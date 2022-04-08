34184f9705c3f2180bac0c9aae8ca0f698eba481
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 17:19:12 +0700

    Изменил наименование поля "Игровое поле" на "gameField"

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -13,7 +13,7 @@ public class Game {
     /**
      * игровое поле
      */
-    private int[][] field;
+    private int[][] gameField;
 
     /**
      * Количество очков
@@ -21,7 +21,7 @@ public class Game {
     private int points;
 
     public Game() {
-        this.field = new int[SIDE_SIZE][SIDE_SIZE];
+        this.gameField = new int[SIDE_SIZE][SIDE_SIZE];
         this.points = 0;
     }
 
@@ -45,19 +45,19 @@ public class Game {
             int position = random.nextInt(SIDE_SIZE << 1);
             int row = position / SIDE_SIZE;
             int column = position % SIDE_SIZE;
-            if (this.field[row][column] == 0)
-                this.field[row][column] = 2;
+            if (this.gameField[row][column] == 0)
+                this.gameField[row][column] = 2;
             else
                 i--;
         }
     }
 
     public int[][] getField() {
-        return field;
+        return gameField;
     }
 
     public void setField(int[][] field) {
-        this.field = field;
+        this.gameField = field;
     }
 
     public int getPoints() {
