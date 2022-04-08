ebfdb9bfa69a592b8efcbe60c69868869bc44247
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 23:21:03 +0700

    Добавил тесты для метода игрового цикла

--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
+
     private final PrintStream standardOut = System.out;
     private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
 
@@ -98,6 +100,167 @@ public class GameTest {
         initGameField.setAccessible(true);
         initGameField.invoke(game);
         assertEquals(PRINTED_ARRAY_STRING, outputStreamCaptor.toString());
+    }
+
+    /**
+     * Тест выхода из игрового цикла. Для выхода в консоль вводится буква "q".
+     * Проверяется сообщение, выводимое при выходе из игры.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void gameCycleQuitTest() throws Exception {
+        String playerInput = "q";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(playerInput.getBytes());
+        System.setIn(byteArrayInputStream);
+        Game game = new Game();
+        Method runCycle = Game.class.getDeclaredMethod("gameCycle");
+        runCycle.setAccessible(true);
+        runCycle.invoke(game);
+        String[] lines = outputStreamCaptor.toString().split(System.lineSeparator());
+        String result = lines[lines.length - 1];
+        String expected = "Спасибо за игру. Ваши очки: 0";
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Тест перемещения чисел на игровом поле вверх в игровом цикле.
+     * Для выхода из игры дополнительно в консоль вводится буква "q".
+     * Проверяется игровое поле и количество очков.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void gameCycleUpMoveTest() throws Exception {
+        String playerInput = "w" + System.lineSeparator() + "q";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(playerInput.getBytes());
+        System.setIn(byteArrayInputStream);
+        int[][] startArray = new int[][]{
+                {2, 4, 2, 4},
+                {8, 8, 4, 2},
+                {16, 2, 16, 8},
+                {8, 2, 2, 4}
+        };
+        Game game = new Game();
+        game.setField(startArray);
+        Method runCycle = Game.class.getDeclaredMethod("gameCycle");
+        runCycle.setAccessible(true);
+        runCycle.invoke(game);
+        int[][] expectedArray = new int[][]{
+                {2, 4, 2, 4},
+                {8, 8, 4, 2},
+                {16, 4, 16, 8},
+                {8, 2, 2, 4}
+        };
+        assertArrayEquals(expectedArray, game.getField());
+        assertEquals(4, game.getPoints());
+    }
+
+    /**
+     * Тест перемещения чисел на игровом поле вниз в игровом цикле.
+     * Для выхода из игры дополнительно в консоль вводится буква "q".
+     * Проверяется игровое поле и количество очков.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void gameCycleDownMoveTest() throws Exception {
+        String playerInput = "s" + System.lineSeparator() + "q";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(playerInput.getBytes());
+        System.setIn(byteArrayInputStream);
+        int[][] startArray = new int[][]{
+                {2, 4, 2, 4},
+                {8, 8, 4, 2},
+                {16, 2, 16, 8},
+                {8, 2, 2, 4}
+        };
+        Game game = new Game();
+        game.setField(startArray);
+        Method runCycle = Game.class.getDeclaredMethod("gameCycle");
+        runCycle.setAccessible(true);
+        runCycle.invoke(game);
+        int[][] expectedArray = new int[][]{
+                {2, 2, 2, 4},
+                {8, 4, 4, 2},
+                {16, 8, 16, 8},
+                {8, 4, 2, 4}
+        };
+        assertArrayEquals(expectedArray, game.getField());
+        assertEquals(4, game.getPoints());
+    }
+
+    /**
+     * Тест проигрышной ситуации в игровом цикле.
+     * Проигрышная ситуация достигается перемещением чисел на игровом поле влево.
+     * Проверяется игровое поле, количество очков и выводимое в консоль сообщение.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void gameCycleLeftMoveWithLossTest() throws Exception {
+        String playerInput = "a";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(playerInput.getBytes());
+        System.setIn(byteArrayInputStream);
+        int[][] startArray = new int[][]{
+                {2, 4, 2, 4},
+                {8, 8, 4, 2},
+                {16, 2, 16, 8},
+                {8, 2, 8, 4}
+        };
+        Game game = new Game();
+        game.setField(startArray);
+        Method runCycle = Game.class.getDeclaredMethod("gameCycle");
+        runCycle.setAccessible(true);
+        runCycle.invoke(game);
+        int[][] expectedArray = new int[][]{
+                {2, 4, 2, 4},
+                {16, 4, 2, 0},
+                {16, 2, 16, 8},
+                {8, 2, 8, 4}
+        };
+        String[] lines = outputStreamCaptor.toString().split(System.lineSeparator());
+        String result = lines[lines.length - 1];
+        String expected = "Поражение. Ваши очки: 16";
+        assertArrayEquals(expectedArray, game.getField());
+        assertEquals(16, game.getPoints());
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Тест выигрышной ситуации в игровом цикле.
+     * Выигрышная ситуация достигается перемещением чисел на игровом поле вправо.
+     * Проверяется игровое поле, количество очков и выводимое в консоль сообщение.
+     *
+     * @throws Exception - исключение рефлексии
+     */
+    @Test
+    void gameCycleRightMoveWithWinTest() throws Exception {
+        String playerInput = "d";
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(playerInput.getBytes());
+        System.setIn(byteArrayInputStream);
+        int[][] startArray = new int[][]{
+                {2, 4, 256, 256},
+                {8, 8, 4, 2},
+                {16, 2, 16, 8},
+                {8, 2, 8, 4}
+        };
+        Game game = new Game();
+        game.setField(startArray);
+        Method runCycle = Game.class.getDeclaredMethod("gameCycle");
+        runCycle.setAccessible(true);
+        runCycle.invoke(game);
+        int[][] expectedArray = new int[][]{
+                {0, 2, 4, 512},
+                {0, 16, 4, 2},
+                {16, 2, 16, 8},
+                {8, 2, 8, 4}
+        };
+        String[] lines = outputStreamCaptor.toString().split(System.lineSeparator());
+        String result = lines[lines.length - 1];
+        String expected = "Победа. Ваши очки: 528";
+        assertArrayEquals(expectedArray, game.getField());
+        assertEquals(528, game.getPoints());
+        assertEquals(expected, result);
     }
 
 }
