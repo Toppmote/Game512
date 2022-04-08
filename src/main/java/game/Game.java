b2041a2c428b3d598a58eac60da1521cf0bd4f1d
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 18:51:23 +0700

    Добавил метод перемещения цифр вверх

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -2,6 +2,7 @@ package game;
 
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
 
 /**
  * Класс игры
@@ -32,9 +33,7 @@ public class Game {
      * Процедура печати правил
      */
     private void printRules() {
-        final String RULES_STRING = "game.Game 512.\n" + "Двигайте цифры вверх вниз влево или право соединяя одинаковые цифры." +
-                "Как только значение одной клетки достигнет 512 - вы победили. Если вы не сможете больше двигать цифры - " +
-                "вы проиграли.\n";
+        final String RULES_STRING = "game.Game 512.\n" + "Двигайте цифры вверх вниз влево или право соединяя одинаковые цифры." + "Как только значение одной клетки достигнет 512 - вы победили. Если вы не сможете больше двигать цифры - " + "вы проиграли.\n";
         System.out.print(RULES_STRING);
     }
 
@@ -48,10 +47,8 @@ public class Game {
             int position = random.nextInt(SIDE_SIZE << 1);
             int row = position / SIDE_SIZE;
             int column = position % SIDE_SIZE;
-            if (this.gameField[row][column] == 0)
-                this.gameField[row][column] = 2;
-            else
-                i--;
+            if (this.gameField[row][column] == 0) this.gameField[row][column] = 2;
+            else i--;
         }
     }
 
@@ -64,36 +61,85 @@ public class Game {
         boolean isMoved = false;
         for (int i = 0; i < SIDE_SIZE; i++) {
             int[] newRow = Arrays.copyOf(this.gameField[i], SIDE_SIZE);
-            boolean isSlided = false;
-            boolean[] isCombined = new boolean[SIDE_SIZE];
-            for (int j = 1; j < SIDE_SIZE; j++) {
-                if (newRow[j] == 0)
-                    continue;
-                while (j != 0) {
-                    if (newRow[j - 1] == 0) {
-                        newRow[j - 1] = newRow[j];
-                        newRow[j--] = 0;
-                        isSlided = true;
-                    } else
-                        break;
-                }
-                if (j == 0)
-                    continue;
-                if (newRow[j - 1] == newRow[j] && !isCombined[j - 1]) {
-                    int newValue = newRow[j] << 1;
-                    newRow[j - 1] = newValue;
-                    newRow[j] = 0;
-                    isCombined[j - 1] = true;
-                    isSlided = true;
-                    points += newValue;
-                }
-            }
+            boolean isSlided = this.slideLeft(newRow);
             if (isSlided) {
                 this.gameField[i] = newRow;
                 isMoved = true;
             }
         }
         return isMoved;
+    }
+
+    /**
+     * Функция сдвигания всех плиток вверх
+     *
+     * @return true - если был сделан сдвиг, false - если сдвига не было
+     */
+    private boolean moveUp() {
+        boolean isMoved = false;
+        for (int i = 0; i < SIDE_SIZE; i++) {
+            int[] newColumn = copyColumn(i);
+            boolean isSlided = this.slideLeft(newColumn);
+            if (isSlided) {
+                this.insertColumn(newColumn, i);
+                isMoved = true;
+            }
+        }
+        return isMoved;
+    }
+
+    /**
+     * Процедура сдвигания всех чисел влево для одномерного массива.
+     *
+     * @param arrayForSlide массив для сдвигания чисел в нем
+     * @return true - если был сделан сдвиг, false - если сдвига не было
+     */
+    private boolean slideLeft(int[] arrayForSlide) {
+        boolean isSlided = false;
+        boolean[] isCombined = new boolean[SIDE_SIZE];
+        for (int j = 1; j < SIDE_SIZE; j++) {
+            if (arrayForSlide[j] == 0) continue;
+            while (j != 0) {
+                if (arrayForSlide[j - 1] == 0) {
+                    arrayForSlide[j - 1] = arrayForSlide[j];
+                    arrayForSlide[j--] = 0;
+                    isSlided = true;
+                } else break;
+            }
+            if (j == 0) continue;
+            if (arrayForSlide[j - 1] == arrayForSlide[j] && !isCombined[j - 1]) {
+                int newValue = arrayForSlide[j] << 1;
+                arrayForSlide[j - 1] = newValue;
+                arrayForSlide[j] = 0;
+                isCombined[j - 1] = true;
+                isSlided = true;
+                points += newValue;
+            }
+        }
+        return isSlided;
+    }
+
+    /**
+     * Функция копирования столбца игрового поля
+     *
+     * @param columnIndex индекс столбца для копирования
+     * @return массив со скопированным столбцом
+     */
+    private int[] copyColumn(int columnIndex) {
+        return IntStream.range(0, SIDE_SIZE)
+                .map(currIndex -> this.gameField[currIndex][columnIndex])
+                .toArray();
+    }
+
+    /**
+     * Процедура вставки столбца в игровое поле
+     *
+     * @param insertableColumn вставляемый столбец
+     * @param columnIndex      индекс для вставки столбца
+     */
+    private void insertColumn(int[] insertableColumn, int columnIndex) {
+        for (int i = 0; i < SIDE_SIZE; i++)
+            this.gameField[i][columnIndex] = insertableColumn[i];
     }
 
     public int[][] getField() {
