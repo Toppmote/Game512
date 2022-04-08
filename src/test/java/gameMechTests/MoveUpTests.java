c9fdaf573976800628c58fa911200ae785c618f6
Pavel Chupikov <toppmote@gmail.com>
2022-04-08 18:13:21 +0700

    Добавил класс для тестирования операции перемещения всех цифр на игровом поле вверх

---
+++ src/test/java/gameMechTests/MoveUpTests.java
@@ -0,0 +1,14 @@
+package gameMechTests;
+
+import game.Game;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Класс для тестирования метода перемещения всех цифр на игровом поле вверх.
+ */
+public class MoveUpTests {
+}
